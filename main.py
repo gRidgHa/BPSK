76def73a1f31fa6ba3db968ca2081b5e9e35e9a7
gRidgHa <max.bit2012@yandex.ru>
2023-04-19 00:44:13 +0300

    доработки 3

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ size = 15
 sampling_t = 0.0005  # шаг в 0.0005 секунду
 sampling_freq = 1 / sampling_t  # частота дискретизации
 n = int(size / sampling_t)
-print(n)
 
 
 w = []
@@ -26,10 +25,10 @@ for iw_freq in range(30000):
     if iw_freq == 0:
         w_freq.append(0)
     else:
-        w_freq.append(w[iw_freq - 1] + sampling_freq / n)
+        w_freq.append(w_freq[iw_freq - 1] + sampling_freq / n)
 
 
-length = 1500  # длина
+length = 15000  # длина
 c = 1500  # Скорость звука
 h = 100  # Глубина 100м
 l = 1  # Номер моды
@@ -52,16 +51,18 @@ for i3 in range(len(q)):
 
 
 # Random generate signal sequence
-a = np.random.randint(0, 2, size)
+a = np.random.randint(0, 2, size * 20) # массив значений 0 и 1
 m = np.zeros(len(w), dtype=np.float32)
+
 for i in range(len(w)):
-    m[i] = a[math.floor(w[i])]
+    m[i] = a[math.floor(w[i] * 20)]
+
 fig = plt.figure()
 ax1 = fig.add_subplot(2, 1, 1)
-
 ax1.set_title('generate Random Binary signal', fontsize=20)
-plt.axis([0, size, -0.5, 1.5])
+plt.axis([0.9, 1.1, -0.5, 1.5])
 plt.plot(w, m, 'b')
+
 # /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 fc = 100  # Несущая частота
 ts = np.arange(0, (30000) / sampling_freq, 1 / sampling_freq)
@@ -73,43 +74,29 @@ bpsk_fft = scipy.fft.fft(bpsk)  # Сигнал bpsk после прямого п
 for i_fft in range(len(bpsk_fft)):
     if isinstance(q[i_fft], complex):
         bpsk_fft[i_fft] = 0
+    if 2 * pi * w_freq[i_fft] / q[i_fft].real + 0.0000000001 > 1700:
+        bpsk_fft[i_fft] = 0
 
-for i_print in range(len(bpsk_fft)):
-    print(str(i_print) + ":" + str(bpsk_fft[i_print]))
-
+#for i_print in range(12000):
+#    print(str(i_print) + ":" + str(bpsk_fft[i_print]) + " " + str(isinstance(q[i_print], complex)))
+#print("7367 " + str(w_freq[7367]))
 sign = []
 
 for i4 in range(len(bpsk_fft)):
     sign.append(bpsk_fft[i4] * (math.e ** (-1j * fi[i4])))  # Спектр принятого сигнала
 sign_ifft = scipy.fft.ifft(sign)  # Принятый сигнал после обратного преобразования Фурье
 
-#print(fi)
-#print("Первые")
-#for i10 in range(len(bpsk_fft)):
-#    if i10 < 5:
-#        print(bpsk_fft[i10])
-#    if i10 == 5:
-#        print("|||||||||||||||||||")
-#        print("Середина")
-#    if i10 > 14997 and i10 < 15003:
-#        print(bpsk_fft[i10])
-#    if i10 == 15002:
-#        print("|||||||||||||||||||")
-#        print("Последние")
-#    if i10 > 29994:
-#        print(bpsk_fft[i10])
 
-# print(bpsk_fft)
 
 # plt.axis([0, size, -1.5, 1.5])
 # plt.plot(t, bpsk_fft)
 # plt.show()
 
 # BPSK modulated signal waveform
-# ax2 = fig.add_subplot(2, 1, 2)
-# ax2.set_title('BPSK Modulation', fontsize=20)  # , fontproperties=zhfont1
-# plt.axis([0, size, -1.5, 1.5])
-# plt.plot(w, bpsk, 'r')
+#ax2 = fig.add_subplot(2, 1, 2)
+#ax2.set_title('BPSK Modulation', fontsize=20)  # , fontproperties=zhfont1
+#plt.axis([0.9, 1.1, -1.5, 1.5])
+#plt.plot(w, bpsk, 'r')
 
 ax3 = fig.add_subplot(2, 1, 2)
 ax3.set_title('sign_ifft', fontsize=20)  # , fontproperties=zhfont1
