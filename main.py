3976f16b64e1f49371c4ec4b77f7d156b348edc6
gRidgHa <max.bit2012@yandex.ru>
2023-05-14 22:26:45 +0300

    Правильно работающий принятый сигнал

--- main.py
+++ main.py
@@ -11,57 +11,66 @@ sampling_t = 0.0005  # шаг в 0.0005 секунду
 sampling_freq = 1 / sampling_t  # частота дискретизации
 n = int(size / sampling_t)
 
+length = 30000  # длина
+c = 1500  # Скорость звука
+h = 100  # Глубина 100м
+l = 1  # Номер моды
 
 w = []
 w_freq = []
-# w = np.arange(0, size, sampling_t)
+k = []
+q = []
+fi = []  # фазовый набег
+new_w = []
+zeroes = int(length / c * (1 / sampling_t)) * 2
+#zeroes = 0
+
 for i0 in range(n):
     if i0 == 0:
         w.append(0)
     else:
         w.append(w[i0 - 1] + sampling_t)
 
-for iw_freq in range(30000):
+for i_new_w in range(n + zeroes):
+    if i_new_w == 0:
+        new_w.append(0)
+    else:
+        new_w.append(new_w[i_new_w - 1] + sampling_t)
+
+for iw_freq in range(n + zeroes):
     if iw_freq == 0:
         w_freq.append(0)
     else:
-        w_freq.append(w_freq[iw_freq - 1] + sampling_freq / n)
-
-
-length = 15000  # длина
-c = 1500  # Скорость звука
-h = 100  # Глубина 100м
-l = 1  # Номер моды
-
-k = []
-q = []
-fi = []  # фазовый набег
+        w_freq.append(w_freq[iw_freq - 1] + sampling_freq / (n + zeroes - 1))
 
 for i1 in range(len(w_freq)):
-    k.append(2 * pi * w_freq[i1] / c)
+    if i1 <= len(w_freq) / 2:
+        k.append(2 * pi * w_freq[i1] / c)
+    else:
+        k.append(2 * pi * w_freq[len(w_freq) - i1] / c)
 
 for i2 in range(len(k)):
     q.append((k[i2] ** 2 - pi ** 2 / h ** 2 * (l - 0.5) ** 2) ** 0.5)
 
-
 for i3 in range(len(q)):
-    fi.append(q[i3] * length)
-
-
-
+    if i3 <= len(q) / 2:
+        fi.append(q[i3] * length)
+    else:
+        fi.append(-1 * q[i3] * length)
 
 # Random generate signal sequence
-a = np.random.randint(0, 2, size * 20) # массив значений 0 и 1
+a = np.random.randint(0, 2, size * 20)  # массив значений 0 и 1
 m = np.zeros(len(w), dtype=np.float32)
 
 for i in range(len(w)):
     m[i] = a[math.floor(w[i] * 20)]
 
-fig = plt.figure()
-ax1 = fig.add_subplot(2, 1, 1)
+fig = plt.figure(figsize=(10, 10))
+ax1 = fig.add_subplot(3, 1, 1)
 ax1.set_title('generate Random Binary signal', fontsize=20)
-plt.axis([0.9, 1.1, -0.5, 1.5])
+plt.axis([0, size, -0.5, 1.5])
 plt.plot(w, m, 'b')
+fig.tight_layout(h_pad=2.5)
 
 # /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 fc = 100  # Несущая частота
@@ -69,38 +78,61 @@ ts = np.arange(0, (30000) / sampling_freq, 1 / sampling_freq)
 coherent_carrier = np.cos(np.dot(2 * pi * fc, ts))
 
 bpsk = np.cos(np.dot(2 * pi * fc, ts) + pi * (m - 1) + pi / 4)
-bpsk_fft = scipy.fft.fft(bpsk)  # Сигнал bpsk после прямого преобразования Фурье
+bpsk = list(bpsk)
 
-for i_fft in range(len(bpsk_fft)):
+for i_zeroes in range(zeroes):  # Добивание нулями
+    bpsk.append(0)
+
+bpsk_fft = scipy.fft.fft(bpsk)  # Сигнал bpsk после прямого преобразования Фурье
+# print(len(bpsk_fft))
+
+
+bpsk_zeroes = 0
+
+for i_fft in range(int(len(bpsk_fft) / 2)):  # занулене
     if isinstance(q[i_fft], complex):
         bpsk_fft[i_fft] = 0
     if 2 * pi * w_freq[i_fft] / q[i_fft].real + 0.0000000001 > 1700:
         bpsk_fft[i_fft] = 0
+    if bpsk_fft[i_fft] == 0:
+        bpsk_zeroes += 1
 
-#for i_print in range(12000):
-#    print(str(i_print) + ":" + str(bpsk_fft[i_print]) + " " + str(isinstance(q[i_print], complex)))
-#print("7367 " + str(w_freq[7367]))
+for i_fft_2 in range(len(bpsk_fft)):
+    if i_fft_2 > len(bpsk_fft) - bpsk_zeroes:
+        bpsk_fft[i_fft_2] = 0
+
 sign = []
 
 for i4 in range(len(bpsk_fft)):
     sign.append(bpsk_fft[i4] * (math.e ** (-1j * fi[i4])))  # Спектр принятого сигнала
+    if i4 == len(bpsk_fft) / 2:
+        sign[i4] = 0
+
+
 sign_ifft = scipy.fft.ifft(sign)  # Принятый сигнал после обратного преобразования Фурье
+# sign_ifft = np.real(sign_ifft)
+
+#for i_printer in range(len(sign_ifft)):
+#   if i_printer < 3:
+#       print(str(i_printer + 1) + ": " + str(sign_ifft[i_printer]))
+#   if len(sign_ifft) / 2 - 2 < i_printer < len(sign_ifft) / 2 + 2:
+#       print(str(i_printer + 1) + ": " + str(sign_ifft[i_printer]))
+#   if i_printer > len(sign_ifft) - 4:
+#       print(str(i_printer + 1) + ": " + str(sign_ifft[i_printer]))
+
+#for i_temp in range(len(sign_ifft)):
+#    if  40000 > i_temp > 20000:
+#        print(str(i_temp) + ":" + str(sign_ifft[i_temp]))
 
 
+ax2 = fig.add_subplot(3, 1, 2)
+ax2.set_title('BPSK Modulation', fontsize=20)  # , fontproperties=zhfont1
+plt.axis([0, 56, -1.5, 1.5])
+plt.plot(new_w, bpsk, 'r')
 
-# plt.axis([0, size, -1.5, 1.5])
-# plt.plot(t, bpsk_fft)
-# plt.show()
-
-# BPSK modulated signal waveform
-#ax2 = fig.add_subplot(2, 1, 2)
-#ax2.set_title('BPSK Modulation', fontsize=20)  # , fontproperties=zhfont1
-#plt.axis([0.9, 1.1, -1.5, 1.5])
-#plt.plot(w, bpsk, 'r')
-
-ax3 = fig.add_subplot(2, 1, 2)
-ax3.set_title('sign_ifft', fontsize=20)  # , fontproperties=zhfont1
-plt.axis([0.9, 1.1, -1.5, 1.5])
-plt.plot(w, sign_ifft, 'r')
+ax3 = fig.add_subplot(3, 1, 3)
+ax3.set_title('Принятый сигнал' + " (" + str(length) + "м)", fontsize=20)  # , fontproperties=zhfont1
+plt.axis([0, 56, -2, 2])
+plt.plot(new_w, sign_ifft, 'g')
 
 plt.show()
